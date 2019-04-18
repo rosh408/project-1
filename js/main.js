ce7d6916bcdd2341ff79e67228553c119496fab2
Mac <mac2018@Macs-iMac-6.local>
2019-04-18 16:51:43 -0700

    Init, after accidental commit of desktop

---
+++ js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+// all of your code goes inside here
+
+console.log('this is working');
+const flkty = new Flickity( '.main-carousel', {
+    cellAlign: 'left',
+    wrapAround: true,
+    freeScroll: true
+  });
+
+
+
+
+  //write soem code here e.g. if statements and try alerting a message
+  //how would you get the value of the email input?
+  //!= '' this line just means that the value is not empty
+  // if(check the input value != '')
+//
+
+
+});
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Aloha Project 1
+
+header text 
+- First list item
+-second list item
+
+## My second section
+
+```
+<h1>Hand crafted</h1>
+
+```
+
+
